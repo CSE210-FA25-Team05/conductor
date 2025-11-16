9a28f3e4e9db8502d482c871d41196a61df71ade
MisakizzZZZ <401058751@qq.com>
2025-11-16 04:36:27 -0800

    format adjustment

--- backend/src/services/auth/index.js
+++ backend/src/services/auth/index.js
@@ -10,12 +10,8 @@ const authRoutes = require('./auth.routes');
 
 module.exports = async function authServicePlugin(fastify) {
   fastify.register(authRoutes);
-  fastify.get(
-    '/me',
-    { preHandler: fastify.authenticate },
-    async (req) => {
-      // req.user is set in decorators/auth.js based on the sid cookie.
-      return req.user;
-    }
-  );
+  fastify.get('/me', { preHandler: fastify.authenticate }, async (req) => {
+    // req.user is set in decorators/auth.js based on the sid cookie.
+    return req.user;
+  });
 };
