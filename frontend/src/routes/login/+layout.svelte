166ea86fe9e258af8bb3b42e93133acb82d7fe78
Justin Radatti <jdradatti@gmail.com>
2025-11-17 10:36:21 -0800

    Center login button

---
+++ frontend/src/routes/login/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	let { children } = $props();
+</script>
+
+<section class="centered">
+	{@render children()}
+</section>
+
+<style>
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
