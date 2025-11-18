61327b291ed1fe6c40d94a3e67d4f4cde2ae9989
Zerlina Lai <76027329+zerlinazl@users.noreply.github.com>
2025-11-18 13:04:37 -0800

    Update LogoutButton.svelte

--- frontend/src/lib/components/LogoutButton.svelte
+++ frontend/src/lib/components/LogoutButton.svelte
@@ -22,3 +22,30 @@
 </script>
 
 <button on:click={logout}>Logout</button>
+
+<style>
+  button {
+    background: #606060;
+    color: white;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    font-weight: 600;
+    display: block;
+    padding: 10px 14px;
+    text-decoration: none;
+    font-size: 0.7em;
+  }
+
+  button:hover {
+    background: #d66;
+  }
+
+  @media (min-width: 800px) {
+    button {
+      position: fixed;
+      bottom: 20px;
+      left: 10px;
+    }
+  }
+</style>
