c983b2c75402a9176c7e5c4f077085ce518c7d89
AneeshJalan <akjalan@ucsd.edu>
2025-11-20 04:54:45 -0800

    expose prisma singleton client for use in tests/seed

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,16 +1,9 @@
 'use strict';
 
 const fp = require('fastify-plugin');
-const { PrismaClient } = require('@prisma/client');
+const prisma = require('./prisma-client');
 
 module.exports = fp(async function prismaPlugin(fastify) {
-  const prisma = new PrismaClient({
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
-  });
-
   // Establish connection
   await prisma.$connect();
 
